5697e2854efd3826cc9daf233be3f25e68303b7e
nhibberd <nhibberd@gmail.com>
2018-03-10 17:48:39 +0700

    Initial project structure

---
+++ src/Seamstress.hs
@@ -0,0 +1,2 @@
+module Seamstress (
+  ) where
---
+++ test/test.hs
@@ -0,0 +1,9 @@
+import           System.IO (BufferMode(..), hSetBuffering, stdout, stderr)
+import           System.Exit (exitFailure)
+
+main :: IO ()
+main = do
+  hSetBuffering stdout LineBuffering
+  hSetBuffering stderr LineBuffering
+
+  exitFailure
