b19349054469500f98c8ea1b35666b3dccf15eee
nhibberd <nhibberd@gmail.com>
2018-03-10 18:08:33 +0700

    Add basic data types

--- src/Seamstress/Data.hs
+++ src/Seamstress/Data.hs
@@ -1,2 +1,26 @@
+{-# LANGUAGE RankNTypes #-}
 module Seamstress.Data (
+    Label(..)
+  , Handler(..)
+  , Supervise(..)
   ) where
+
+import           Data.Text (Text)
+
+newtype Label =
+  Label {
+      label :: Text
+    } deriving (Eq, Show)
+
+data Handler =
+    Terminate
+  | RestartForever
+  | Restart Int
+    deriving (Eq, Show)
+
+data Supervise a =
+  Supervise {
+      superviseLabel :: Label
+    , superviseHandler :: Handler
+    , superviseAction :: a
+    }
